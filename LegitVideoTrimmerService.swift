db5a7fcc7d75c2bf1d6f9aa9a72739baa73d9596
Andrii Novoselskyi <anovoselskyi@gmail.com>
2020-08-28 12:47:35 +0300

    Implemented LegitVideoTrimmerService

---
+++ LegitVideoTrimmerService.swift
@@ -0,0 +1,55 @@
+//
+//  LegitVideoTrimmerService.swift
+//  react-native-legit-video-trimmer
+//
+//  Created by Andrii Novoselskyi on 28.08.2020.
+//
+
+import AVFoundation
+
+class LegitVideoTrimmerService {
+    
+    func trimVideo(asset: AVAsset, startTime: Double, endTime: Double, completion: ((_ outputUrl: URL) -> Void)? = nil) {
+        let fileManager = FileManager.default
+        let documentDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+
+        let length = Float(asset.duration.value) / Float(asset.duration.timescale)
+        print("video length: \(length) seconds")
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'"
+        let date = Date()
+
+        var outputURL = documentDirectory.appendingPathComponent("output")
+        do {
+            try fileManager.createDirectory(at: outputURL, withIntermediateDirectories: true, attributes: nil)
+            outputURL = outputURL.appendingPathComponent("\(formatter.string(from: date)).mp4")
+        }catch let error {
+            print(error)
+        }
+
+        //Remove existing file
+        try? fileManager.removeItem(at: outputURL)
+
+        guard let exportSession = AVAssetExportSession(asset: asset, presetName: AVAssetExportPresetHighestQuality) else { return }
+        exportSession.outputURL = outputURL
+        exportSession.outputFileType = .mp4
+
+        let timeRange = CMTimeRange(start: CMTime(seconds: startTime, preferredTimescale: 1000),
+                                    end: CMTime(seconds: endTime, preferredTimescale: 1000))
+
+        exportSession.timeRange = timeRange
+        exportSession.exportAsynchronously {
+            switch exportSession.status {
+            case .completed:
+                print("exported at \(outputURL)")
+                completion?(outputURL)
+            case .failed:
+                print("failed \(exportSession.error.debugDescription)")
+            case .cancelled:
+                print("cancelled \(exportSession.error.debugDescription)")
+            default: break
+            }
+        }
+    }
+}
--- LegitVideoTrimmerView.swift
+++ LegitVideoTrimmerView.swift
@@ -101,6 +101,8 @@ class LegitVideoTrimmerView: UIView {
     private var playbackTimeCheckerTimer: Timer?
     private var trimmerPositionChangedTimer: Timer?
     
+    private var videoTrimmerService = LegitVideoTrimmerService()
+    
     init() {
         super.init(frame: .zero)
         
@@ -180,7 +182,7 @@ class LegitVideoTrimmerView: UIView {
     @objc
     private func doneButtonDidPress(button: UIButton) {
         guard let asset = asset else { return }
-        trimVideo(asset: asset, startTime: trimmerView.startTime!.seconds, endTime: trimmerView.endTime!.seconds) { url in
+        videoTrimmerService.trimVideo(asset: asset, startTime: trimmerView.startTime!.seconds, endTime: trimmerView.endTime!.seconds) { url in
             print("Result url: \(url)")
         }
     }
@@ -282,52 +284,5 @@ extension LegitVideoTrimmerView: TrimmerViewDelegate {
         player?.seek(to: playerTime, toleranceBefore: CMTime.zero, toleranceAfter: CMTime.zero)
         let duration = (trimmerView.endTime! - trimmerView.startTime!).seconds
         selectedDurationLabel.text = String(format: kSelectedDurationFormat, duration)
-    }
-}
-
-extension LegitVideoTrimmerView {
-    
-    func trimVideo(asset: AVAsset, startTime: Double, endTime: Double, completion: ((_ outputUrl: URL) -> Void)? = nil) {
-        let fileManager = FileManager.default
-        let documentDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
-
-        let length = Float(asset.duration.value) / Float(asset.duration.timescale)
-        print("video length: \(length) seconds")
-        
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'"
-        let date = Date()
-
-        var outputURL = documentDirectory.appendingPathComponent("output")
-        do {
-            try fileManager.createDirectory(at: outputURL, withIntermediateDirectories: true, attributes: nil)
-            outputURL = outputURL.appendingPathComponent("\(formatter.string(from: date)).mp4")
-        }catch let error {
-            print(error)
-        }
-
-        //Remove existing file
-        try? fileManager.removeItem(at: outputURL)
-
-        guard let exportSession = AVAssetExportSession(asset: asset, presetName: AVAssetExportPresetHighestQuality) else { return }
-        exportSession.outputURL = outputURL
-        exportSession.outputFileType = .mp4
-
-        let timeRange = CMTimeRange(start: CMTime(seconds: startTime, preferredTimescale: 1000),
-                                    end: CMTime(seconds: endTime, preferredTimescale: 1000))
-
-        exportSession.timeRange = timeRange
-        exportSession.exportAsynchronously {
-            switch exportSession.status {
-            case .completed:
-                print("exported at \(outputURL)")
-                completion?(outputURL)
-            case .failed:
-                print("failed \(exportSession.error.debugDescription)")
-            case .cancelled:
-                print("cancelled \(exportSession.error.debugDescription)")
-            default: break
-            }
-        }
     }
 }
